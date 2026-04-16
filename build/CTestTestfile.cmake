27907be795b776b92c9a807e4a2c2adb464ce738
User Name <user@example.com>
2026-04-16 10:26:15 +0200

    feat(slice): implementar flujo C++ y tests (Entrada, parseo, aplicación, integración)

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/Uned/lenguajes de  programacion/pregunta 4
+# Build directory: E:/Uned/lenguajes de  programacion/pregunta 4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(all_tests "E:/Uned/lenguajes de  programacion/pregunta 4/build/tests_runner.exe")
+set_tests_properties(all_tests PROPERTIES  _BACKTRACE_TRIPLES "E:/Uned/lenguajes de  programacion/pregunta 4/CMakeLists.txt;17;add_test;E:/Uned/lenguajes de  programacion/pregunta 4/CMakeLists.txt;0;")
